73c8cf1be2e05f970edab6a5e83c3b0b98b93770
mmilillo <manuelmilillo@gmail.com>
2019-09-26 23:05:07 -0300

    commit inicial codigo y simulacion

---
+++ Vueltas_por_tiempo/Vueltas_por_tiempo.ino
@@ -0,0 +1,40 @@
+const int sensorPin = 9;
+const int puentePin = 8;
+int contador = 0;
+unsigned long tiempo = 0;
+unsigned long  tiempo_anterior = 0;
+int ciclos = 0;
+int valor_anterior = 0;
+int value = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT); 
+}
+
+void loop ()
+{
+  value = digitalRead(sensorPin);
+
+  if(value != valor_anterior)
+  {
+    contador ++;
+    valor_anterior = value;
+  }
+
+  tiempo = millis();
+
+  if(tiempo - tiempo_anterior >= 5000)
+  {
+    tiempo_anterior = tiempo;
+
+    ciclos = contador; /// 42; //24 rendijas una vuelta, 42 cambios 
+    Serial.println(ciclos);
+
+    contador = 0;
+    
+  }
+  delay(1); 
+}
+
