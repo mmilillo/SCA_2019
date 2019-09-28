b9d52cf7334bd5f9fb3041462c06aba497a9f61f
mmilillo <manuelmilillo@gmail.com>
2019-09-28 11:37:47 -0300

    Se agrega codigo para aceleracion

---
+++ Controlar_Aceleracion/Controlar_Aceleracion.ino
@@ -0,0 +1,56 @@
+const int sensorPin = 9;
+const int puentePin = 8;
+int contador = 0;
+unsigned long tiempo = 0;
+unsigned long  tiempo_anterior = 0;
+int ciclos = 0;
+int valor_anterior = 0;
+int value = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT); 
+}
+
+void loop ()
+{
+  value = digitalRead(sensorPin);
+
+  if(value != valor_anterior)
+  {
+    contador ++;
+    valor_anterior = value;
+  }
+
+  tiempo = millis();
+
+  if(tiempo - tiempo_anterior >= 5000)
+  {
+    tiempo_anterior = tiempo;
+
+    ciclos = contador; /// 42; //24 rendijas una vuelta, 42 cambios 
+    Serial.println(ciclos);
+
+
+    if(ciclos > 5)
+    {
+      Serial.println("apagate");
+      contador = 0;
+      digitalWrite(puentePin, LOW);
+       
+    }
+    else
+    {
+      contador = 0;
+      Serial.println("prendete");
+      digitalWrite(puentePin, HIGH);
+    }
+    
+    
+    contador = 0;
+    
+  }
+  delay(1); 
+}
+
